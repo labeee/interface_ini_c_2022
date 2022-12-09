3a2afd82d8167fc295931dcd189f02fe098473cb
arturkamimura <58118744+arturkamimura@users.noreply.github.com>
2022-12-09 09:28:13 -0300

    Add files via upload

--- js/modal_componentes.js
+++ js/modal_componentes.js
@@ -28,19 +28,25 @@ function add_vidro(){
 	else{
 		var u_vidro = document.getElementById('u_vidro').value
 		var fs_vidro = document.getElementById('valor_fs').value
-		var dict = {nome: nome_vidro, u_vid: u_vidro, fs_vid: fs_vidro}
-		vidros.push(dict)
-
-		//função para atualizar os vidros nas listas:
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selvidros = document.getElementById("lista_vidros"+(i));
-		vidro_a_adicionar = vidros[vidros.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = vidro_a_adicionar.nome;
-		selvidros.appendChild(optionInc);
+		if([u_vidro, fs_vidro].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_vidro, u_vid: u_vidro, fs_vid: fs_vidro}
+			vidros.push(dict)
+
+			//função para atualizar os vidros nas listas:
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selvidros = document.getElementById("lista_vidros"+(i));
+			vidro_a_adicionar = vidros[vidros.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = vidro_a_adicionar.nome;
+			selvidros.appendChild(optionInc);
+			}
+		}
+		
 	}
 	
 }
@@ -58,19 +64,25 @@ function add_vidro_az(){
 	else{
 		var u_vidro = document.getElementById('u_vidro_az').value
 		var fs_vidro = document.getElementById('valor_fs_az').value
-		var dict = {nome: nome_vidro, u_vid: u_vidro, fs_vid: fs_vidro}
-		vidros_az.push(dict)
-
-		//função para atualizar os vidros nas listas:
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selvidros = document.getElementById("lista_vidros_az"+(i));
-		vidro_a_adicionar = vidros[vidros.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = vidro_a_adicionar.nome;
-		selvidros.appendChild(optionInc);
+		if([u_vidro, fs_vidro].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_vidro, u_vid: u_vidro, fs_vid: fs_vidro}
+			vidros_az.push(dict)
+	
+			//função para atualizar os vidros nas listas:
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selvidros = document.getElementById("lista_vidros_az"+(i));
+			vidro_a_adicionar = vidros[vidros.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = vidro_a_adicionar.nome;
+			selvidros.appendChild(optionInc);
+			}
+		}
+	
 	}
 	
 }
@@ -90,19 +102,25 @@ function add_parede_externa(){
 		var u_parede = document.getElementById('u_parede_ext').value
 		var ct_parede = document.getElementById('ct_parede_ext').value
 		var ars_parede = document.getElementById('ars_parede_ext').value
-		var dict = {nome: nome_parede, u_par: u_parede, ct_par: ct_parede, ars_par: ars_parede}
-		paredes_externas.push(dict)
-
-		//função para atualizar as paredes nas listas:
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selparedes = document.getElementById("lista_paredes_ext"+(i));
-		parede_a_adicionar = paredes_externas[paredes_externas.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = parede_a_adicionar.nome;
-		selparedes.appendChild(optionInc);
+		if ([u_parede, ct_parede, ars_parede].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_parede, u_par: u_parede, ct_par: ct_parede, ars_par: ars_parede}
+			paredes_externas.push(dict)
+
+			//função para atualizar as paredes nas listas:
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selparedes = document.getElementById("lista_paredes_ext"+(i));
+			parede_a_adicionar = paredes_externas[paredes_externas.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = parede_a_adicionar.nome;
+			selparedes.appendChild(optionInc);
+			}
+		}
+		
 	}
 }
 
@@ -120,19 +138,25 @@ function add_parede_interna(){
 	else{
 		var u_parede = document.getElementById('u_parede_int').value
 		var ct_parede = document.getElementById('ct_parede_int').value
-		var dict = {nome: nome_parede, u_par: u_parede, ct_par: ct_parede, }
-		paredes_internas.push(dict)
-
-		//função para atualizar as paredes nas listas:
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selparedes = document.getElementById("lista_paredes_int"+(i));
-		parede_a_adicionar = paredes_internas[paredes_internas.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = parede_a_adicionar.nome;
-		selparedes.appendChild(optionInc);
+		if ([u_parede, ct_parede].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_parede, u_par: u_parede, ct_par: ct_parede, }
+			paredes_internas.push(dict)
+
+			//função para atualizar as paredes nas listas:
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selparedes = document.getElementById("lista_paredes_int"+(i));
+			parede_a_adicionar = paredes_internas[paredes_internas.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = parede_a_adicionar.nome;
+			selparedes.appendChild(optionInc);
+			}
+		}
+		
 	}
 }
 
@@ -150,19 +174,24 @@ function add_cob(){
 		var u_cobertura = document.getElementById('u_cob').value
 		var ct_cobertura = document.getElementById('ct_cob').value
 		var ars_cobertura = document.getElementById('ars_cob').value
-		var dict = {nome: nome_cob, u_cob: u_cobertura, ct_cob: ct_cobertura, ars_cob: ars_cobertura}
-		coberturas.push(dict)
-		
-		//função para atualizar as coberturas nas listas
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selcobs = document.getElementById("lista_coberturas"+(i));
-		cob_a_adicionar = coberturas[coberturas.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = cob_a_adicionar.nome;
-		selcobs.appendChild(optionInc);
+		if ([u_cobertura, ct_cobertura, ars_cobertura].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_cob, u_cob: u_cobertura, ct_cob: ct_cobertura, ars_cob: ars_cobertura}
+			coberturas.push(dict)
+			
+			//função para atualizar as coberturas nas listas
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selcobs = document.getElementById("lista_coberturas"+(i));
+			cob_a_adicionar = coberturas[coberturas.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = cob_a_adicionar.nome;
+			selcobs.appendChild(optionInc);
+			}
+		}	
 	}
 }
 
@@ -180,19 +209,25 @@ function add_piso(){
 	else{
 		var u_piso = document.getElementById('u_piso').value
 		var ct_piso = document.getElementById('ct_piso').value
-		var dict = {nome: nome_piso, u_piso: u_piso, ct_piso: ct_piso}
-		pisos.push(dict)
-		
-		//função para atualizar as pisos nas listas
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var selpisos = document.getElementById("lista_pisos"+(i));
-		piso_a_adicionar = pisos[pisos.length - 1];
-		var optionInc = document.createElement("OPTION");
-		optionInc.innerHTML = piso_a_adicionar.nome;
-		selpisos.appendChild(optionInc);
+		if ([u_piso, ct_piso].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_piso, u_piso: u_piso, ct_piso: ct_piso}
+			pisos.push(dict)
+			
+			//função para atualizar as pisos nas listas
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var selpisos = document.getElementById("lista_pisos"+(i));
+			piso_a_adicionar = pisos[pisos.length - 1];
+			var optionInc = document.createElement("OPTION");
+			optionInc.innerHTML = piso_a_adicionar.nome;
+			selpisos.appendChild(optionInc);
+			}
+		}
+		
 	}
 }
 
@@ -212,61 +247,25 @@ function add_ac(){
 		var tipo_coeficiente= document.getElementById('tipo_cee').value
 		var valor_coeficiente = document.getElementById('valor_cee').value
 		var pot_renovacao = document.getElementById('renovacao_ac').value
-		var dict = {nome: nome_ac, tipo:tipo_ac, capacidade: capacidade_ac, tipo_cee: tipo_coeficiente, valor_cee:valor_coeficiente, potencia_renovacao:pot_renovacao}
-		sistemas_ac.push(dict)
-
-		//função para atualizar os sistemas de AC nas listas:
-		var rowCount = $('#myTable tr').length;
-		var rowCount = rowCount - 1
-		for (var i = 0; i < rowCount; i++) {
-		var sel_ac = document.getElementById("ac_zona"+(i));
-		ac_a_adicionar = sistemas_ac[sistemas_ac.length - 1];
-		var optionInc510 = document.createElement("OPTION");
-		optionInc510.innerHTML = ac_a_adicionar.nome;
-		sel_ac.appendChild(optionInc510);
+		if ([tipo_ac, capacidade_ac, tipo_coeficiente, valor_coeficiente, pot_renovacao].includes('')){
+			window.alert('Todos os campos devem ser preenchidos para que o componente seja adicionado')
 		}
+		else{
+			var dict = {nome: nome_ac, tipo:tipo_ac, capacidade: capacidade_ac, tipo_cee: tipo_coeficiente, valor_cee:valor_coeficiente, potencia_renovacao:pot_renovacao}
+			sistemas_ac.push(dict)
+
+			//função para atualizar os sistemas de AC nas listas:
+			var rowCount = $('#myTable tr').length;
+			var rowCount = rowCount - 1
+			for (var i = 0; i < rowCount; i++) {
+			var sel_ac = document.getElementById("ac_zona"+(i));
+			ac_a_adicionar = sistemas_ac[sistemas_ac.length - 1];
+			var optionInc510 = document.createElement("OPTION");
+			optionInc510.innerHTML = ac_a_adicionar.nome;
+			sel_ac.appendChild(optionInc510);
+			}
+		}
+		
 	}
 }
-
-/*
-function add_pav(){
-    var nome_pavimento = document.getElementById('nome_pav').value
-	var dict = {nome: nome_pavimento, }
-	pavimentos.push(dict)
-
-	//função para atualizar os pavimentos nas listas:
-	var rowCount = $('#myTable tr').length;
-	var rowCount = rowCount - 1
-	for (var i = 0; i < rowCount; i++) {
-	var selpavs = document.getElementById("lista_pavimentos"+(i));
-	pav_a_adicionar = pavimentos[pavimentos.length - 1];
-	var optionInc = document.createElement("OPTION");
-	optionInc.innerHTML = pav_a_adicionar.nome;
-	selpavs.appendChild(optionInc);
-	}
-}
-
-
-function add_ab(){
-    var nome_abertura = document.getElementById('nome_ab').value
-	var uvid_abertura = document.getElementById('uvid_ab').value
-	var fs_abertura = document.getElementById('fs_ab').value
-	var paf_abertura = document.getElementById('paf_ab').value
-	var avs_abertura = document.getElementById('avs_ab').value
-	var ahs_abertura = document.getElementById('ahs_ab').value
-	var aov_abertura = document.getElementById('aov_ab').value
-	var dict = {nome: nome_abertura, uvid: uvid_abertura, fs_vid: fs_abertura, PAF: paf_abertura, avs: avs_abertura, ahs: ahs_abertura, aov: aov_abertura,}
-	aberturas.push(dict)
-	
-	//função para atualizar as aberturas nas listas:
-	var rowCount = $('#myTable tr').length;
-	var rowCount = rowCount - 1
-	for (var i = 0; i < rowCount; i++) {
-	var selabs = document.getElementById("lista_aberturas"+(i));
-	ab_a_adicionar = aberturas[aberturas.length - 1];
-	var optionInc = document.createElement("OPTION");
-	optionInc.innerHTML = ab_a_adicionar.nome;
-	selabs.appendChild(optionInc);
-	}
-}*/
 
